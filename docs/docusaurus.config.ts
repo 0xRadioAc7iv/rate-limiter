af1bf66dd3f2e103601475fa2d681b8b1af06fac
Manav <manav18gadhiya@gmail.com>
2025-02-28 13:53:34 +0530

    update URL

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://0xradioac7iv.github.io/rate-limiter/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
